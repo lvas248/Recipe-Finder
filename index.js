ae1759d26454664a9efe20bf2532c956d9034fc2
Luis <lvas248@gmail.com>
2022-07-15 15:50:25 -0400

    Project Plan

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+//Header: Let user search for a recipe
+    //create a button to generate a random recipe from the DB
+
+//body: contains 3 panels: Left panel, Center panel, and Right panel
+    //left panel: When user searc
+        //Event Listener 'click': User clicks recipe result and it loads to showPanel
+
+    //center panel - 
+        //Event Listener DOMContentLoad: display top rate recipe to showPanel
+        //comment&like section
+
+    //Right Panel - when content loads, displays top 10 rated recipes
+
+//Stretch Goal: create a local server to keep track of likes, comments, maybe user submitted recipes
+
+        
