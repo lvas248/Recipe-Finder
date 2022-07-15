f3a35433264f38f15e90f9acebcbde485bd881d0
Luis <lvas248@gmail.com>
2022-07-15 17:24:54 -0400

    Add a 'change' event listener and functionality to the first drop down option - Header

--- index.js
+++ index.js
@@ -13,4 +13,36 @@
 
 //Stretch Goal: create a local server to keep track of likes, comments, maybe user submitted recipes
 
-        
+
+//HEADER Functionality---------
+document.addEventListener('DOMContentLoaded', (e)=>{
+    const dropDown = document.querySelector('#dropDown')
+    const secondDropDown = document.querySelector('#secondaryDropDown')
+    const searchInput = document.querySelector('#searchInput')
+    function getSecondDropDownDetails(){
+        secondDropDown.innerHTML= ""
+        fetch(`https://www.themealdb.com/api/json/v1/1/list.php?${dropDown.value[0]}=list`)
+        .then(res => res.json())
+        .then(data => {
+            const chimi = 'str' + dropDown.value[0].toUpperCase() + dropDown.value.substring(1)
+            data.meals.forEach(meal =>{
+                secondDropDown.innerHTML += `<option value=${meal[chimi]}>${meal[chimi]}</option>`
+            })
+        })
+    }
+
+    dropDown.addEventListener('change', (e)=>{
+        if(dropDown.value === "search"){
+            searchInput.style.display = "inline-block"   
+        }
+        else if(dropDown.value === 'area' || dropDown.value === 'category'){
+            searchInput.style.display = "none"
+            secondDropDown.style.display = "inline-block"
+            getSecondDropDownDetails()
+        }
+    })
+
+})
+
+
+
